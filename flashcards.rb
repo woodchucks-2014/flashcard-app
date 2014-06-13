2be856058da633cd9998117967f69fccb4e78e86
Michael Drew Teter & Antonio Perez <pair+mdrewt.rantoniops@devbootcamp.com>
2014-06-13 11:20:47 -0400

    initial commit

---
+++ flashcards.rb
@@ -0,0 +1,15 @@
+require 'sqlite3'
+
+=begin
+deck class
+store several cards
+pick a card at random
+return the card.question
+get the card.answer
+decide whether to move one to next card
+
+card class
+has a question
+has an answer
+can return question or answer
+verify the answer == card.answer
